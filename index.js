7078ad1865b5ce0c197b3e0add2e87c6d26124ef
Jee Yoon Lee <jlee66@wellesley.edu>
2018-05-02 08:10:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 function happyHolidays(){
   return "Happy holidays!"
 }
-
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
 function happyHolidayTo(name, holiday){
   return `Happy ${holiday}, ${name}!`
 }
